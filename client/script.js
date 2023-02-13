10826d0f1bff54ab64f8c22025f04485c4ca992c
David Do <dohuytoan@live.com>
2023-02-13 13:37:28 +0700

    update

--- client/script.js
+++ client/script.js
@@ -79,7 +79,7 @@ const uniqueId = generateUniqueID()
 
   // fetch data from the server
 
-  const response = await fetch('https://chatwithjd.onrender.com/', {
+  const response = await fetch('https://chatwithcodex.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
